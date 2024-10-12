1a083763f61f4d30aa3d69f2f3ef9bd0f2f39235
Simone Seria <simoneseria@live.com>
2024-10-12 16:47:39 +0200

    aggiunte esperienza 3

--- 3. Garage Band/main.py
+++ 3. Garage Band/main.py
@@ -3,16 +3,15 @@ import soundfile as sf
 import numpy as np
 
 if __name__ == '__main__':
-    file = "diapason.wav"
+    file = "3. Garage Band/diapason.wav"
     data, samplerate = sf.read(file)
     if np.ndim(data) > 1:
         data = np.mean(data, axis=1)
     print(data)
-    # sf.write("pulita_semplice_mono.wav", data, samplerate)
+    sf.write("3. Garage Band/pulita_semplice_mono.wav", data, samplerate)
     Time = np.linspace(0, len(data) / samplerate, num=len(data))
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
     plt.title(file)
     plt.plot(Time, data)
-    plt.show()
-
+    plt.show()   
