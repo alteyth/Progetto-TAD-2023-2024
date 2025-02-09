8becc21026d48217e550dc944f851875d43a7864
Simone Seria <simoneseria@live.com>
2025-02-10 00:01:48 +0100

    varie

--- 4. Trasformata 2D di Immagini/main.py
+++ 4. Trasformata 2D di Immagini/main.py
@@ -5,7 +5,7 @@ import scipy.signal as signal
 import matplotlib.image as image
 
 if __name__ == '__main__':
-    image = image.imread("4. Trasformata 2D di Immagini/immagini/line_0deg.png")
+    image = image.imread("4. Trasformata 2D di Immagini/immagini/line_90deg.png")
     image = np.mean(image, axis=2)
 
     image_fft = fft.fft2(image)
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     image_ps = np.abs(image_fft)
     image_ps_shifted = np.abs(image_fft_shifted)
     
-    plt.imshow(image_ps, norm="log")
+    plt.imshow(image_ps_shifted, norm="log")
     plt.xlabel('Frequenza Righe')
     plt.ylabel('Frequenza Colonne')
 
